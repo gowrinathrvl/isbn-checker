96c2e25752b6065abad5e27dbdac50830285bf84
Gowrinath ragannagari venkata lakshmi <gowrinath.rvl@gmail.com>
2024-04-05 18:48:36 -0400

    ISBN checker

---
+++ src/main/java/ISBNCheck.java
@@ -0,0 +1,46 @@
+public class ISBNCheck {
+
+    public static final int LONG_ISBN = 13;
+    public static final int SHORT_ISBN = 10;
+    public static final int LONG_ISBN_CHECK_DIGIT_MULTIPLIER = 10;
+    public static final int SHORT_ISBN_CHECK_DIGIT_MULTIPLIER = 11;
+
+    public boolean checkISBN(String  isbn) {
+        if(isbn.length() == LONG_ISBN){
+            return isValid13DigitISBN(isbn);
+        }else if(isbn.length() == SHORT_ISBN){
+            return isValid10DigitISBN(isbn);
+        }
+        throw new NumberFormatException("ISBN should be 10 digits only");
+    }
+
+    private static boolean isValid10DigitISBN(String isbn) {
+        int total =0;
+        for(int i=0; i<SHORT_ISBN; i++){
+            if(!Character.isDigit(isbn.charAt(i))){
+                if(i == 9 && isbn.charAt(i) == 'X'){
+                    total += 10;
+                }else{
+                    throw new NumberFormatException("ISBN should be numeric");
+                }
+            }else{
+                total += Character.getNumericValue(isbn.charAt(i)) * (SHORT_ISBN-i);
+            }
+        }
+       return total % SHORT_ISBN_CHECK_DIGIT_MULTIPLIER == 0;
+
+    }
+
+    private static boolean isValid13DigitISBN(String isbn) {
+        int total = 0;
+        for(int i= 0; i<LONG_ISBN; i++){
+            if(i%2 == 0){
+                total += Character.getNumericValue(isbn.charAt(i));
+            }else{
+                total += Character.getNumericValue(isbn.charAt(i)) * 3;
+            }
+        }
+        return total % LONG_ISBN_CHECK_DIGIT_MULTIPLIER == 0;
+
+    }
+}
---
+++ src/test/java/ISBNCheckTest.java
@@ -0,0 +1,77 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ISBNCheckTest {
+    //Valid ISBN  140177396
+    @Test
+    public void checkValidShortISBN(){
+        //Given
+        ISBNCheck isbnCheck = new ISBNCheck();
+        //When
+       boolean result = isbnCheck.checkISBN("0140449116");
+       //Then
+        assertTrue("First ISBN",result);
+
+        result = isbnCheck.checkISBN("0140177396");
+        assertTrue("Second ISBN",result);
+
+
+    }
+   @Test
+    public void checkInvalidShotISBN(){
+        //Given
+        ISBNCheck isbnCheck = new ISBNCheck();
+        //When
+        boolean result = isbnCheck.checkISBN("0140449110");
+        //Then
+        assertFalse(result);
+
+    }
+    @Test(expected = NumberFormatException.class)
+    public void lessThanOrGreaterThan10Invalid(){
+        //Given
+        ISBNCheck isbnCheck = new ISBNCheck();
+        //when
+        isbnCheck.checkISBN("123456789");
+        //then throw numbeFormatException
+
+    }
+    @Test(expected = NumberFormatException.class)
+    public void alphabeticalCharsInvalid(){
+        //Given
+        ISBNCheck isbnCheck = new ISBNCheck();
+        //When
+        isbnCheck.checkISBN("helloWorld");
+    }
+    @Test
+    public void checkValidISBNEndingWithX(){
+        //Given
+        ISBNCheck isbnCheck = new ISBNCheck();
+        //When
+        boolean result = isbnCheck.checkISBN("140274577X");
+        //Then
+        assertTrue(result);
+
+    }
+    //valid 13 digit 9780345538376
+    @Test
+    public void checkValidLongISBN(){
+        //Given
+        ISBNCheck isbnCheck = new ISBNCheck();
+        //When
+        boolean result = isbnCheck.checkISBN("9780345538376");
+        //Then
+        assertTrue("13digit ISBN",result);
+    }
+    @Test
+    public void checkInvalidLongISBN(){
+        //Given
+        ISBNCheck isbnCheck = new ISBNCheck();
+        //When
+        boolean result = isbnCheck.checkISBN("9780345538370");
+        //Then
+        assertFalse(result);
+
+    }
+}
